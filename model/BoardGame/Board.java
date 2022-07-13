1d27e085e2caed510460478fe50b0e78ac9e4008
Alessandro Guilherme <alessandroglopes01@gmail.com>
2022-07-13 19:58:36 -0300

    BoardException and defensive programming

--- model/BoardGame/Board.java
+++ model/BoardGame/Board.java
@@ -10,6 +10,9 @@ public class Board {
     }
 
     public Board(Integer rows, Integer columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("ERROR CREATING BOARD: THERE MUST BE AT LEAST 1 ROW AND 1 COLUMN");
+        }
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
@@ -20,31 +23,54 @@ public class Board {
         return rows;
     }
 
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
-
     public Integer getColumns() {
         return columns;
     }
 
-    public void setColumns(Integer columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column) {
+
+        if (!positionExists(row, column)) {
+            throw new BoardException("POSITION NOT ON THE BOARD");
+        }
         return pieces[row][column];
 
     }
 
     public Piece piece(Position position) {
+
+        if (!positionExists(position)) {
+            throw new BoardException("POSITION NOT ON THE BOARD");
+        }
         return pieces[position.getRow()][position.getColumn()];
 
     }
 
     public void placePiece(Piece piece, Position position) {
+
+        if (thereAPiece(position)) {
+            throw new BoardException("THERE IS ALREADY A PIECE ON POSITION");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+
+    }
+
+    public boolean thereAPiece(Position position) {
+
+        if (!positionExists(position)) {
+            throw new BoardException("POSITION NOT ON THE BOARD");
+        }
+        return piece(position) != null;
+
     }
 
 }
---
+++ model/BoardGame/BoardException.java
@@ -0,0 +1,10 @@
+package model.boardGame;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String msg) {
+
+        super(msg);
+    }
+
+}
